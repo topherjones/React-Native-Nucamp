5e6ebee400e8415418c0a5450c7d163cb210aeff
tophfinch <tophfinch@gmail.com>
2020-02-12 14:42:59 -0700

    Using Redux

--- nucampsite/components/AboutComponent.js
+++ nucampsite/components/AboutComponent.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { ScrollView,  Text, FlatList } from 'react-native';
-import { PARTNERS } from '../shared/partners';
 import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners 
+    };
+};
 
 function Mission() {
     return (
@@ -20,12 +28,6 @@ function Mission() {
 }
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -38,7 +40,7 @@ class About extends Component {
                 <ListItem
                     title= {item.name}
                     subtitle= {item.description}
-                    leftAvatar= {{source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar= {{source: {uri: baseUrl + item.image}}}
                 />
             );
         }
@@ -48,7 +50,7 @@ class About extends Component {
                 <Mission />
                 <Card title='Community Partners'>
                     <FlatList 
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         keyExtractor={item => item.id.toString()}
                         renderItem={renderPartner}
                         />
@@ -58,4 +60,4 @@ class About extends Component {
     }
 }
 
-export default About; 
+export default connect(mapStateToProps)(About); 
