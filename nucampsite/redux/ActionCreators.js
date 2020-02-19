2585c03d2f3ff381fecadd360c1c0e0bcc38f1d3
tophfinch <tophfinch@gmail.com>
2020-02-18 17:11:59 -0700

    Swipeout and Delete Favorites

--- nucampsite/redux/ActionCreators.js
+++ nucampsite/redux/ActionCreators.js
@@ -166,6 +166,11 @@ export const addFavorite = campsiteId => ({
   payload: campsiteId 
 });
 
+export const deleteFavorite = campsiteId => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: campsiteId
+});
+
 export const addComment = comment => ({
   type: ActionTypes.ADD_COMMENT,
   payload: comment
